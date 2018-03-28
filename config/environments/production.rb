7da1b05874ea8b4c7c8d7cae1a9124ccc2236ea8
Rahil <rahil.brainium@gmail.com>
2018-03-28 16:46:27 +0530

    mailer config changed in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,14 +78,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'https://desolate-plains-66489.herokuapp.com'}
-  ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.gmail.com',
-  :domain         => 'mail.google.com',
-  :port           => 587,
-  :user_name      => 'rahil.brainium@gmail.com',
-  :password       => 'rojinadutta123',
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
+  config.action_mailer.delivery_method = :smtp
+   config.action_mailer.smtp_settings = {
+     address: 'smtp.mail.com',
+     port: 587,
+     domain: 'https://desolate-plains-66489.herokuapp.com/',
+     user_name: 'rahil.max41@gmail.com',
+     password: 'iphone4s8gb',
+     authentication: 'plain',
+     enable_starttls_auto: true }
 end
